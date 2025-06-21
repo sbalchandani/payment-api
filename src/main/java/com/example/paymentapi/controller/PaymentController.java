e5385857fc337fda704fdbc2006f08ae29fb57dc
Sailesh Balchandani <saileshdb@hotmail.com>
2025-06-20 21:09:11 -0500

    first commit

--- src/main/java/com/example/paymentapi/controller/PaymentController.java
+++ src/main/java/com/example/paymentapi/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.example.paymentapi.controller;
 import com.example.paymentapi.entity.Payment;
 import com.example.paymentapi.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,8 @@ public class PaymentController {
     @PostMapping
     public ResponseEntity<Payment> submitPayment(@RequestBody Payment payment) {
         Payment savedPayment = paymentRepository.save(payment);
-        return ResponseEntity.ok(savedPayment);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        return ResponseEntity.ok().headers(headers).body(savedPayment);
     }
 }
